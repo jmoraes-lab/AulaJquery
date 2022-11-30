518729ee2af140c27e630ee68793f7740c43a6d7
Joseph <elderluan.m@gmail.com>
2022-11-30 19:32:17 -0400

    Alterados

--- js/cadastrar.js
+++ js/cadastrar.js
@@ -4,7 +4,13 @@ botaoCadastro.addEventListener('click', function (e) {
         nome: document.querySelector('#nome').value,
         contato: document.querySelector('#contato').value,
         data: document.querySelector('#data').value,
-        hora: document.querySelector('#hora').value
+        hora: document.querySelector('#hora').value,
+        cep: $('#cep').val(),
+        logradouro: $('logradouro').val(),
+        numero: $('numero').val(),
+        bairro: $('bairro').val(),
+        cidade: $('cidade').val(),
+        uf: $('#uf').val()
 
     }
     fetch('https://api-aluno.meuapp.net.br/api/eventos', {
@@ -19,4 +25,18 @@ botaoCadastro.addEventListener('click', function (e) {
             alert('Deu ruim');
             console.log(error)
         })
+});
+$('#cep').on('change', function () {
+    var cep = $(this).val();
+    var rota = `https://viacep.com.br/ws/${cep}/json/`
+    $.ajax({
+        url: rota,
+        success: function (resposta) {
+            $('#bairro').val(resposta.bairro);
+            $('#logradouro').val(resposta.logradouro);
+            $('#numero').val(resposta.numero);
+            $('#cidade').val(resposta.localidade);
+            $('#uf').val(resposta.uf);
+        }
+    })
 })
