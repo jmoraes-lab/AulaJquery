3af4d29370fc528d5faff68866b752505e55d9f3
Joseph <elderluan.m@gmail.com>
2022-11-30 19:02:39 -0400

    Atualizado

--- js/listar.js
+++ js/listar.js
@@ -8,9 +8,11 @@ fetch('https://api-aluno.meuapp.net.br/api/eventos', {
         dados.forEach(function (item) {
             document.querySelector('#dados').innerHTML += `
             <tr>
+            <td><button class="btn btn-danger">Apagar</button></td>
             <td>${item.nome}</td>
             <td>${item.contato}</td>
             <td>${item.data} - ${item.hora}</td>
+            <td>${item.endereco.logradouro}, ${item.endereco.bairro}</td>
             </tr>
             `;
         })
